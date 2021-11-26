69e2e722970250f6d9f574f67e56012d1a68c204
arbaretaz.aurelien <aurelien.arbaretaz@orange.fr>
2021-11-26 11:35:23 +0100

    init with sqlite database

---
+++ src/main/java/aionDB/Main.java
@@ -0,0 +1,108 @@
+package aionDB;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author sqlitetutorial.net
+ */
+public class Main {
+
+	public static Connection connect() {
+        Connection conn = null;
+        try {
+            // db parameters
+
+            String url = "jdbc:sqlite:D:/sqlite/connect/chinook.db";
+            // create a connection to the database
+            conn = DriverManager.getConnection(url);
+            
+            System.out.println("Connection to SQLite has been established.");
+            
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+		return conn;
+    }
+    /**
+     * Connect to a sample database
+     *
+     * @param fileName the database file name
+     */
+    public static void createNewDatabase(String fileName) {
+
+        String url = "jdbc:sqlite:D:/sqlite/connect/" + fileName;
+
+        try (Connection conn = DriverManager.getConnection(url)) {
+            if (conn != null) {
+                DatabaseMetaData meta = conn.getMetaData();
+                System.out.println("The driver name is " + meta.getDriverName());
+                System.out.println("A new database has been created.");
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    public static void createNewTable() {
+        // SQLite connection string
+        String url = "jdbc:sqlite:D:/sqlite/connect/tests.db";
+        
+        // SQL statement for creating a new table
+        String sql = "CREATE TABLE IF NOT EXISTS warehouses (\n"
+                + "	id integer PRIMARY KEY,\n"
+                + "	name text NOT NULL,\n"
+                + "	capacity real\n"
+                + ");";
+        
+        try (Connection conn = DriverManager.getConnection(url);
+                Statement stmt = conn.createStatement()) {
+            // create a new table
+            stmt.execute(sql);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    public void selectAll(){
+        String sql = "SELECT id, name, capacity FROM warehouses";
+        
+        try (Connection conn = this.connect();
+             Statement stmt  = conn.createStatement();
+             ResultSet rs    = stmt.executeQuery(sql)){
+            
+            // loop through the result set
+            while (rs.next()) {
+                System.out.println(rs.getInt("id") +  "\t" + 
+                                   rs.getString("name") + "\t" +
+                                   rs.getDouble("capacity"));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    	 InsertApp app = new InsertApp();
+         // insert three new rows
+         app.insert("Raw Materials", 3000);
+         app.insert("Semifinished Goods", 4000);
+         app.insert("Finished Goods", 5000);
+         SelectApp app2 = new SelectApp();
+         app2.selectAll();
+    }
+}
