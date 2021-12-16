28b3bd74deb8926b28f247afe16351dcd59ba5de
arbaretaz.aurelien <aurelien.arbaretaz@orange.fr>
2021-12-16 11:43:49 +0100

    Binary write and read

--- src/main/java/services/BinaryService.java
+++ src/main/java/services/BinaryService.java
@@ -1,8 +1,40 @@
 package services;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
 public class BinaryService {
 	
-	public void openBinaryFile() {
-		
-	}
+		public  void openBinaryFile()
+	    {
+	        Path path = Paths.get("doc.txt");
+	        byte[] bytes = "11/04/2021,14,25.5;12/05/2020,10,22.3;".getBytes(StandardCharsets.UTF_8);
+	        
+	        try {
+	            Files.write(path, bytes);    
+	            System.out.println("Successfully written data to the file");
+	        }
+	        catch (IOException e) {
+	            e.printStackTrace();
+	        }
+	        try {
+	            File myObj = new File("doc.txt");
+	            Scanner myReader = new Scanner(myObj);
+	            while (myReader.hasNextLine()) {
+	              String data = myReader.nextLine();
+	              System.out.println(data);
+	            }
+	            myReader.close();
+	          } catch (FileNotFoundException e) {
+	            System.out.println("An error occurred.");
+	            e.printStackTrace();
+	          }
+	    }
+	
 }
